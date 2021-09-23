f019f928379fb25bd74f5c6f9b7e739c3a138287
Zabirov Marat Ilsurovich <92zabirovmi@region.cbr.ru>
2021-09-23 13:57:00 +0300

    create easy chain script

--- main.cpp
+++ main.cpp
@@ -4,17 +4,26 @@ std::string helloWorld(const std::string &t_name) {
   return "Hello " + t_name + "!";
 }
 
+size_t SizeOf(const std::string &data) {
+  return data.length();
+}
+
 int main() {
   chaiscript::ChaiScript chai;
   chai.add(chaiscript::fun(&helloWorld), "helloWorld");
+  chai.add(chaiscript::fun(&SizeOf), "SizeOf");
 
   std::string filename = "create_wallet.chai";
-  chai.eval_file("./" + filename);
+  chai.eval_file("./chai-code/" + filename);
 //   chai.eval_file("transaction.chai");
 
 //   chai.eval(R"(
 //     puts(helloWorld("Bob"));
 //   )");
-
+struct MyStruct{uint64_t hash = 777;} my_struct;
+auto str = std::string( reinterpret_cast<char*>(&my_struct), sizeof(my_struct) );
+for (const auto& it : str)
+  printf("%d\n", it);
+  // std::cout << "My struct bytecode" << (uint8_t)it << std::endl;
 
 }
